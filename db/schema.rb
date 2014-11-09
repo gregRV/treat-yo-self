2d852c237e85532eb34dfc35da4d2c9401a5f3bb
Greg RV <gregvarias@gmail.com>
2014-11-08 23:46:17 -0800

    added 'tix' column to User. User show page has links to create a new task or new treat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108075908) do
+ActiveRecord::Schema.define(version: 20141109071416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141108075908) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "tix",             default: 0
   end
 
 end
